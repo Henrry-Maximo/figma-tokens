ca2debcb6146be9f7669a68469ed9f4d0972e243
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-12 14:28:19 -0300

    🎉Intial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Guide
+
+- npm init -y
+- npx style-dictionary init basic
+- npm i style-dictionary
+- npm run build
+- "build": "style-dictionary build"
+- https://amzn.github.io/style-dictionary/#/
---
+++ build/js/tokens.js
@@ -0,0 +1,20 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  "ColorBlack": "#000000",
+  "ColorWhite": "#ffffff",
+  "ColorSky600": "#0284c7",
+  "ColorSky900": "#0c4a6e",
+  "ColorZinc800": "#27272a",
+  "ColorGray600": "#4b5563",
+  "EffectShadow": {"shadowType":"dropShadow","radius":10,"color":"#00000040","offsetX":0,"offsetY":0,"spread":4},
+  "VariableCollectionGray900": "#18181b",
+  "VariableCollectionWhite": "#ffffff",
+  "VariableCollectionBlack800": "#27272a",
+  "VariableCollectionBlack900": "#18181b",
+  "VariableCollectionBlue600": "#0284c7",
+  "VariableCollectionBlue900": "#0369a1",
+  "VariableCollectionGay100": "#4b5563"
+};
